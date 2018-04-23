f857d365a7474c25cf58aa778cfd7f3f8202921b
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-23 12:59:03 +0200

    Setup

---
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pro4.smartbarbell;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+       private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button = (Button) findVievbyID(R.id.stats);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //public void onClick(View v) {
+                openStats();
+            }
+        });
+
+
+    }
+
+    public void openStats(){
+        Intend intent = new Intend(this, Stats.class);
+        startActivity(intent);
+    }
+
+}
