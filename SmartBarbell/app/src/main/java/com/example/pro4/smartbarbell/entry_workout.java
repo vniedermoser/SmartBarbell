26411301530c28cc4478790272b2407711f67421
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-24 13:05:05 +0200

    Datei Namen Fixes

---
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/entry_workout.java
@@ -0,0 +1,14 @@
+package com.example.pro4.smartbarbell;
+
+public class entry_workout {
+
+    private String mWorkoutEntry;
+
+    public entry_workout(String workoutEntry){
+        mWorkoutEntry = workoutEntry;
+    }
+
+    public String getWorkoutEntry(){
+        return mWorkoutEntry;
+    }
+}
