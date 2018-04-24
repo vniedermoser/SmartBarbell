b8dd1d5520c510e7343688d60c4200744ae5fb80
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-24 11:38:39 +0200

    Workout Entry List

---
+++ SmartBarbell/app/src/main/java/entry_workout.java
@@ -0,0 +1,12 @@
+public class entry_workout {
+
+    private String mWorkoutEntry;
+
+    public entry_workout(String workoutEntry){
+        mWorkoutEntry = workoutEntry;
+    }
+
+    public String getWorkoutEntry(){
+        return mWorkoutEntry;
+    }
+}
