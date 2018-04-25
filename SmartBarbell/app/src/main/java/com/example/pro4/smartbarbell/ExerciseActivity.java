76e107da024872ce4fa328b11fd859f829c8c56e
RaffaelaPichler <raffaela.pichler@gmx.at>
2018-04-25 15:03:26 +0200

    Exercise Activity

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/ExerciseActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/ExerciseActivity.java
@@ -2,94 +2,133 @@ package com.example.pro4.smartbarbell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.os.Handler;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ExerciseActivity extends AppCompatActivity {
+
+    TextView textView ;
+    Button start, pause, reset, lap ;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds ;
+    ListView listView ;
+    String[] ListElements = new String[] {  };
+    List<String> ListElementsArrayList ;
+    ArrayAdapter<String> adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        textView = (TextView)findViewById(R.id.txtTimer);
+        start = (Button)findViewById(R.id.btnStart);
+        pause = (Button)findViewById(R.id.btnPause);
+        reset = (Button)findViewById(R.id.btnReset);
+        lap = (Button)findViewById(R.id.btnLap) ;
+        listView = (ListView)findViewById(R.id.listview1);
+
+        handler = new Handler() ;
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(ExerciseActivity.this,
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListElementsArrayList.add(textView.getText().toString());
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
     }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
 }
-
-/*
- package com.example.pro4.smartbarbell;
-
- import android.os.Bundle;
- import android.support.annotation.Nullable;
- import android.support.v4.app.Fragment;
- import android.view.LayoutInflater;
- import android.view.View;
- import android.view.ViewGroup;
-
- import android.os.SystemClock;
- import android.widget.ArrayAdapter;
- import android.widget.Button;
- import android.widget.ListView;
- import android.widget.TextView;
- import android.os.Handler;
- import java.util.Arrays;
- import java.util.List;
- import java.util.ArrayList;
-
- public class AccountActivity extends Fragment {
-
- //Variables
- TextView txtTimer;
- Button start;
- long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
- Handler handler;
- int Seconds, Minutes, MilliSeconds ;
-
- @Nullable
- @Override
- public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
- View view = inflater.inflate(R.layout.account, container, false);
-
-
- //initialize Variables
- txtTimer = (TextView)view.findViewById(R.id.txtTimer);
- start = (Button)view.findViewById(R.id.btnStart);
-
- handler = new Handler() ;
-
- start.setOnClickListener(new View.OnClickListener() {
- @Override
- public void onClick(View view) {
- StartTime = SystemClock.uptimeMillis();
- handler.postDelayed(runnable, 0);
- }
- });
-
-
- return inflater.inflate(R.layout.account, container, false);
- };
-
- public Runnable runnable = new Runnable() {
-
- public void run() {
-
- MillisecondTime = SystemClock.uptimeMillis() - StartTime;
-
- UpdateTime = TimeBuff + MillisecondTime;
-
- Seconds = (int) (UpdateTime / 1000);
-
- Minutes = Seconds / 60;
-
- Seconds = Seconds % 60;
-
- MilliSeconds = (int) (UpdateTime % 1000);
-
- txtTimer.setText("" + Minutes + ":"
- + String.format("%02d", Seconds) + ":"
- + String.format("%03d", MilliSeconds));
-
- handler.postDelayed(this, 0);
- }
-
- };
- }
-
-*/
