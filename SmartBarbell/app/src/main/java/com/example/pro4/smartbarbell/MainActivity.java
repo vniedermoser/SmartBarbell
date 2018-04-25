84c44f3c95836f5c31314138b2162b670f96cfda
RaffaelaPichler <raffaela.pichler@gmx.at>
2018-04-25 16:48:33 +0200

    Revert "Main repair"
    This reverts commit da631aff6eba9b5f422cc97be422711bdfb404af.

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/MainActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/MainActivity.java
@@ -10,9 +10,12 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< HEAD
 import com.google.android.gms.auth.api.signin.GoogleSignInApi;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+=======
+>>>>>>> parent of d11c9b2... Merge branch 'master' of https://github.com/vniedermoser/SmartBarbell
 
 public class MainActivity extends AppCompatActivity {
 
@@ -123,6 +126,10 @@ public class MainActivity extends AppCompatActivity {
     // -------------------
     // end basic structure
     // -------------------
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> parent of da631af... Main repair
 /*
     private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -152,4 +159,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 */
+=======
+    };*/
+
+>>>>>>> parent of d11c9b2... Merge branch 'master' of https://github.com/vniedermoser/SmartBarbell
 }
