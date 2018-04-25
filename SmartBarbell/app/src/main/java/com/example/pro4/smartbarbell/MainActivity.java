839726e7a6d68b0dd212a645557543caf5385492
RaffaelaPichler <raffaela.pichler@gmx.at>
2018-04-25 16:17:54 +0200

    Buttons für activities

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/MainActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pro4.smartbarbell;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(TAG, "On Create .....");
 
-        setOnClickListener(new View.OnClickListener(){
+        Button button1 = (Button)findViewById(R.id.ResultListActivity);
+        Button button2 = (Button)findViewById(R.id.TimerActivity);
 
-                                 }
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ExerciseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // ---
         /*
