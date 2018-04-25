a2a6c6540bbf1075c57df9b1c88494b140d9ebaa
RaffaelaPichler <raffaela.pichler@gmx.at>
2018-04-25 16:35:22 +0200

    Revert "Merge branch 'master' of https://github.com/vniedermoser/SmartBarbell"
    This reverts commit d11c9b2ad3d45d4f08238e550327f817a6d969aa, reversing
    changes made to a2b7c838f96496591f55bb4e6bd3d56a82b5d550.

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/ExerciseActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/ExerciseActivity.java
@@ -8,7 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
