9ea12da8581d769ee9627ef382a34cb16476f0c4
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-26 13:11:34 +0200

    Toolbar Accountt

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
@@ -3,6 +3,7 @@ package com.example.pro4.smartbarbell;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +14,9 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
 
 
