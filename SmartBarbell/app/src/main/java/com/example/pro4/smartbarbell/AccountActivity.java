ef05bc6182713289832bf5c0a27ae345048637c5
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-26 13:43:12 +0200

    Fix Backup

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
@@ -13,6 +13,7 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        /*Fix 26.04.13:40*/
 
 
 
