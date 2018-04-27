0802971e947760d235f39caa05b99676dbb26efb
Verena <s1610238061@students.fh-hagenberg.at>
2018-04-27 10:03:07 +0200

    Toolbar top+bottom

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/SettingsActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/SettingsActivity.java
@@ -1,7 +1,14 @@
 package com.example.pro4.smartbarbell;
 
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +16,60 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
+
+
+        ImageButton calendar = findViewById(R.id.set_calendar) ;
+        ImageButton workout = findViewById(R.id.set_workout) ;
+        ImageButton nfc = findViewById(R.id.set_nfc) ;
+        ImageButton stats = findViewById(R.id.set_stats) ;
+        ImageButton account = findViewById(R.id.set_account) ;
+        Button main = findViewById(R.id.set_settings);
+
+        main.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        calendar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        workout.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, WorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nfc.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        stats.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, StatsActivity.class);
+                startActivity(intent);
+            }
+        });
+        account.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
