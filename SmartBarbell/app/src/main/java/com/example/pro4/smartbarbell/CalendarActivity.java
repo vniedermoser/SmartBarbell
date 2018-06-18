7910cf06411202c4cd6a89a49e5d1b0ba8598b92
Verena <s1610238061@students.fh-hagenberg.at>
2018-06-18 22:40:08 +0200

    Calendar

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/CalendarActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/CalendarActivity.java
@@ -1,18 +1,56 @@
 package com.example.pro4.smartbarbell;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
-public class CalendarActivity extends AppCompatActivity {
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class CalendarActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+
+
+        Spinner mon = findViewById(R.id.spinnerMON);
+        Spinner tue = findViewById(R.id.spinnerTUE);
+        Spinner wed = findViewById(R.id.spinnerWED);
+        Spinner thu = findViewById(R.id.spinnerTHU);
+        Spinner fri = findViewById(R.id.spinnerFRI);
+        Spinner sat = findViewById(R.id.spinnerSAT);
+        Spinner sun = findViewById(R.id.spinnerSUN);
+
+
+//item liste für spinner --> TODO: strings aus json auslesen & speichern
+        String[] items = new String[]{"-------------","push ups", "deadlifts", "excercise3"};
+//welches item wird agezeigt
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        mon.setAdapter(adapter);
+        tue.setAdapter(adapter);
+        wed.setAdapter(adapter);
+        thu.setAdapter(adapter);
+        fri.setAdapter(adapter);
+        sat.setAdapter(adapter);
+        sun.setAdapter(adapter);
+
+
+
+
+
+
 
         //-----------------
         //----navigation---
@@ -58,6 +96,9 @@ public class CalendarActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
+
 
 }
