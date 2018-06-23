e37c55fb54bce76cec5b45c191b664cb44b4337a
MrGatzi <martingatzweiler@gmail.com>
2018-06-23 20:37:34 +0200

    Icons  for tests

--- SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
+++ SmartBarbell/app/src/main/java/com/example/pro4/smartbarbell/AccountActivity.java
@@ -1,17 +1,12 @@
 package com.example.pro4.smartbarbell;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class AccountActivity extends AppCompatActivity {
 
